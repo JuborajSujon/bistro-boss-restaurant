e326e6495ebe2162619c413c739981184bf09f59
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-16 17:12:06 +0600

    simple router setup

--- bistro-client/src/main.jsx
+++ bistro-client/src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import routes from "./Routes/Routes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
