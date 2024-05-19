f4c3b01cdf13f4b5fce35b856073e47b0b976f80
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-19 13:26:32 +0600

    Connect mongo db

--- bistro-server/index.js
+++ bistro-server/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@smdeveloper.7rzkdcv.mongodb.net/?retryWrites=true&w=majority&appName=SMDeveloper`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.log);
 
 app.get("/", (req, res) => {
   res.send("Bistro Boss Server Running");
