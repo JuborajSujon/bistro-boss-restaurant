9a5dfd8d82dff9cbd19b1588a4de5855921e19c9
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-30 22:12:40 +0600

    updata recommenbs section

--- bistro-client/src/pages/Home/ChefRecommenbs/ChefCard.jsx
+++ bistro-client/src/pages/Home/ChefRecommenbs/ChefCard.jsx
@@ -1,16 +1,15 @@
-const ChefCard = () => {
+const ChefCard = ({ item }) => {
+  const { name, image, price, recipe } = item;
   return (
     <div className="card  bg-base-100 rounded-none">
       <figure className="w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-          className=""
-        />
+        <img src={image} alt={name} className="" />
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{name}</h2>
+        <p>Price : {price}</p>
+        <p>{recipe}</p>
+
         <div className="card-actions">
           <button className="btn btn-outline border-0 border-b-4 border-yellow-500 mt-4 uppercase">
             Add to Cart
--- bistro-client/src/pages/Home/ChefRecommenbs/ChefRecommenbs.jsx
+++ bistro-client/src/pages/Home/ChefRecommenbs/ChefRecommenbs.jsx
@@ -1,17 +1,25 @@
-import React from "react";
 import SectionTitle from "../../../Components/SectionTitle";
 import ChefCard from "./ChefCard";
+import useMenu from "../../../hooks/useMenu";
 
 const ChefRecommenbs = () => {
+  const [menu, loading] = useMenu();
+  const popularItems = menu.filter((item) => item.category === "popular");
+  if (loading) {
+    return (
+      <div className="w-24 mx-auto">
+        <span className="loading loading-bars w-24"></span>
+      </div>
+    );
+  }
   return (
     <div>
       <SectionTitle subHeading="Should Try" heading="Chef Recommenbs" />
 
       <div className="grid md:grid-cols-3 gap-10">
-        <ChefCard />
-        <ChefCard />
-        <ChefCard />
-        <ChefCard />
+        {popularItems.map((item) => (
+          <ChefCard key={item._id} item={item} />
+        ))}
       </div>
     </div>
   );
